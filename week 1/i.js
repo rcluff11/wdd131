fec13ed41bb0bc6d9517ab2d0abbe39ad20f8754
rcluff11 <cluffriley11@gmail.com>
2026-01-07 12:50:42 -0700

    first commit

---
+++ week 1/i.js
@@ -0,0 +1,27 @@
+const PI = 3.14
+let radius = 3
+
+let area = radius * radius * PI
+console.log(area)
+
+radius = 20
+area = radius * radius * PI
+console.log(area)
+
+radus = 50
+
+area = radius * radius * PI
+console.log(area);
+
+console.log(radus)
+
+
+let course = "CSE131"; //global scope
+if (true) {
+    let student = "John";
+    console.log(course);  //works just fine, course is global
+    console.log(student); //works just fine, it's being accessed within the block
+}
+console.log(course); //works fine, course is global
+console.log(student); //does not work, can't access a block variable outside the block
+                    
